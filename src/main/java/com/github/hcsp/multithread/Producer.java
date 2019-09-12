d2f194df4b9199fecb914ead0a139bb98ec418dd
Yongwang Zhang <40329182+zhangyongwang4444@users.noreply.github.com>
2019-09-12 21:20:34 +0800

    实现一个生产者-消费者模型 (#10)
    实现一个生产者-消费者模型 (#10)

--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,40 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Producer extends Thread {
+
+    Container container;
+    ReentrantLock lock;
+
+    Producer(Container container, ReentrantLock lock) {
+        this.container = container;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                lock.lock();
+                while (container.getContainer().isPresent()) {
+                    try {
+                        container.getNotConsumedYet().await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                container.setContainer(Optional.of(r));
+
+               container.getNotProducedYet().signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
 }
