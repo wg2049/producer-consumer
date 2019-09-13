49f465bbdf442ecf628696183909b5c0b489cdb8
JayChenFE <jaychenfe@foxmail.com>
2019-09-13 21:35:09 +0800

    Jay homework blockingqueue (#13)
    Jay homework blockingqueue (#13)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,26 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.IntStream;
+
 public class Consumer extends Thread {
+    private BlockingQueue<Integer> queue;
+    private BlockingQueue<Integer> signalQueue;
+
+    public Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        IntStream.rangeClosed(1, 10).forEach(x -> {
+            try {
+                System.out.println("Consuming " + queue.take());
+                signalQueue.put(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,30 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.stream.IntStream;
+
 public class Producer extends Thread {
+
+    private BlockingQueue<Integer> queue;
+    private BlockingQueue<Integer> signalQueue;
+
+    public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        IntStream.rangeClosed(1, 10).forEach(x -> {
+            try {
+                Integer randomInt = new Random().nextInt();
+                System.out.println("Producing " + randomInt);
+                queue.put(randomInt);
+                signalQueue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+    }
 }
