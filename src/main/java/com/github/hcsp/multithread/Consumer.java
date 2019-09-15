6587a357619a4976e7c3f5fb58d670d4ecf71797
XiongAmao <onycat@live.cn>
2019-09-15 21:31:29 +0800

    XiongAmao homework blocking queue.

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,28 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingDeque;
+
 public class Consumer extends Thread {
+    private final BlockingDeque<Integer> queue;
+    private final BlockingDeque<Integer> signalQueue;
+
+    public Consumer(BlockingDeque<Integer> queue, BlockingDeque<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            int random = new Random().nextInt();
+            try {
+                System.out.println("Producing " + random);
+                queue.put(random);
+                signalQueue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,26 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+
 public class Producer extends Thread {
+    private final BlockingQueue<Integer> queue;
+    private final BlockingDeque<Integer> signalQueue;
+
+    public Producer(BlockingQueue<Integer> queue, BlockingDeque<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println("Consuming " + queue.take());
+                signalQueue.put(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
