8a32cceeeba9da0a14ba3a209a80c2ef26b211fc
XiongAmao <onycat@live.cn>
2019-09-15 21:54:17 +0800

    XiongAmao homework wait/notify. (#16)
    XiongAmao homework wait/notify. (#16)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,33 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+
 public class Consumer extends Thread {
+    private final Object lock;
+    private final Container container;
+
+    public Consumer(Object lock, Container container) {
+        this.lock = lock;
+        this.container = container;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (lock) {
+                while (!container.getValue().isPresent()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Integer random = container.getValue().get();
+                System.out.println("Consuming " + random);
+                container.setValue(Optional.empty());
+                lock.notify();
+            }
+        }
+    }
 }
