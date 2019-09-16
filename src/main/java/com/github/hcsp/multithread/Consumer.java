ae6a6ee421e33079c3e38992f316cf3b8dc1f1bb
dsying <dingduoduo0916@gmail.com>
2019-09-16 23:51:07 +0800

    ReentryantLock/Condition (#17)
    ReentryantLock/Condition (#17)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,34 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Consumer extends Thread {
-    @Override
-    public void run() {}
+  ReentrantLock lock;
+  Container container;
+
+  public Consumer(ReentrantLock lock, Container container) {
+    this.lock = lock;
+    this.container = container;
+  }
+
+  @Override
+  public void run() {
+    try {
+      lock.lock();
+      for (int i = 0; i < 10; i++) {
+        while (container.getOptional().isPresent()) {
+          // 当前容器有值
+          System.out.println("Consuming " + container.getOptional().get());
+          container.setOptional(Optional.empty());
+          container.getNotProducedYet().signal();
+        }
+        container.getNotConsumedYet().await();
+      }
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } finally {
+      lock.unlock();
+    }
+  }
 }
