42d5e96faebd5bab36de80d5a02ef9a76c66a184
GavinYau@1995 <37453766+GavinYau@users.noreply.github.com>
2019-09-24 05:27:29 +0800

    test (#20)
    test (#20)

--- src/main/java/com/github/hcsp/multithread/Boss.java
+++ src/main/java/com/github/hcsp/multithread/Boss.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Boss {
     public static void main(String[] args) throws InterruptedException {
         // 请实现一个生产者/消费者模型，其中：
@@ -14,9 +18,10 @@ public class Boss {
         // Consuming 10086
         // Producing -12345678
         // Consuming -12345678
-
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        ArrayBlockingQueue<Integer> store = new ArrayBlockingQueue<>(1);
+        BlockingQueue<Integer> signalQueue = new LinkedBlockingQueue<>();
+        Producer producer = new Producer(store, signalQueue);
+        Consumer consumer = new Consumer(store, signalQueue);
 
         producer.start();
         consumer.start();
--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,28 @@
 package com.github.hcsp.multithread;
 
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Consumer extends Thread {
+    private ArrayBlockingQueue<Integer> store;
+    private BlockingQueue<Integer> signalQueue;
+
+    public Consumer(ArrayBlockingQueue<Integer> store, BlockingQueue<Integer> signalQueue) {
+        this.store = store;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println("Consuming " + store.take());
+                signalQueue.put(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
