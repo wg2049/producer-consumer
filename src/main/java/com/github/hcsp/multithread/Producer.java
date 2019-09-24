95ae749ab5982fc4a6d6994f52342110d910c690
slTrust <25893245+slTrust@users.noreply.github.com>
2019-09-24 22:10:48 +0800

    Lock/Condition (#23)
    Lock/Condition (#23)

--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,41 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Producer extends Thread {
+    Container container;
+    ReentrantLock lock;
+
+    public Producer(Container container, ReentrantLock lock) {
+        this.container = container;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                lock.lock();
+                // 只要盘子里有东西 它就必须等待
+                while (container.getValue().isPresent()) {
+                    try {
+                        container.getNotConsumedYet().await();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // 盘子是空的就生产
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                container.setValue(Optional.of(r));
+                // 唤醒消费者
+                container.getNotProducedYet().signal();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
 }
