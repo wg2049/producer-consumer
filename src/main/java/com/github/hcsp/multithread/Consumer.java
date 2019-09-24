a2654f0be43a516801733883d287034c11d417d4
slTrust <25893245+slTrust@users.noreply.github.com>
2019-09-24 22:25:47 +0800

    BlockingQueue (#24)
    BlockingQueue (#24)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,26 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingDeque;
+
 public class Consumer extends Thread {
+    BlockingDeque<Integer> queue;
+    BlockingDeque<Integer> signalQueue;
+
+    public Consumer(BlockingDeque<Integer> queue, BlockingDeque<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println("Consuming " + queue.take());
+                signalQueue.put(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
+
