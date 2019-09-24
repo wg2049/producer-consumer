db6b9ec0d592d390e955744e88cc5961b783ee9e
huangjiaxi <trustfor@sina.cn>
2019-09-24 21:34:23 +0800

    Object.wait/notify

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,38 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+
 public class Consumer extends Thread {
+    Container container;
+    Object lock;
+
+    public Consumer(Container container, Object lock) {
+        this.container = container;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (lock) {
+                // 只要盘子是空的 它就等待
+                while (!container.getValue().isPresent()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // 盘子不是空的就消费
+                Integer value = container.getValue().get();
+                // 清空盘子的内容
+                container.setValue(Optional.empty());
+                System.out.println("Consuming " + value);
+
+                // 唤醒生产者
+                lock.notify();
+            }
+        }
+    }
 }
