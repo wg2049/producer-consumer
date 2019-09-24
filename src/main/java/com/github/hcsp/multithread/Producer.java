680ded9d65ad9d6448239d606270e0f996c73b72
slTrust <25893245+slTrust@users.noreply.github.com>
2019-09-24 21:35:24 +0800

    Object.wait/notify (#22)
    Object.wait/notify (#22)

--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,37 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class Producer extends Thread {
+    Container container;
+    Object lock;
+
+    public Producer(Container container, Object lock) {
+        this.container = container;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (lock) {
+                // 只要盘子里有东西 它就必须等待
+                while (container.getValue().isPresent()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // 盘子是空的就生产
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                container.setValue(Optional.of(r));
+                // 唤醒消费者
+                lock.notify();
+            }
+        }
+    }
 }
