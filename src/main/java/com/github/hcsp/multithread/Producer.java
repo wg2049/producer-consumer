28f129dd974fe3d0ba5b28507dd96eb35281777d
Zain <zainking2016@hotmail.com>
2019-10-02 01:22:48 +0800

    Finish Producer&Customer modal

--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,35 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+import java.util.Random;
+
 public class Producer extends Thread {
+    Container container;
+    Object lock;
+
+    Producer (Container container, Object lock) {
+        this.container = container;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (lock) {
+                while (container.getValue().isPresent()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                Integer num = new Random().nextInt();
+                container.setValue(Optional.of(num));
+
+                System.out.println("Producing " + num);
+                lock.notify();
+            }
+        }
+    }
 }
