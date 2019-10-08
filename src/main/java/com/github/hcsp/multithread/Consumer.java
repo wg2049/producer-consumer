310f133f9bdfdc847a68761f11f9cfd060b118a7
hukaibo <52575259+hukaibo@users.noreply.github.com>
2019-10-08 13:25:38 +0800

    生产者消费者模型 (#27)
    生产者消费者模型 (#27)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,34 @@
 package com.github.hcsp.multithread;
 
+import java.util.Optional;
+
 public class Consumer extends Thread {
+    Container container;
+    Object lock;
+
+    public Consumer(Container container, Object lock) {
+        this.container = container;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+
+
+            synchronized (lock) {
+                while (!container.getValue().isPresent()) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Integer value = container.getValue().get();
+                container.setValue(Optional.empty());
+                System.out.println("Consuming " + value);
+                lock.notify();
+            }
+        }
+    }
 }
