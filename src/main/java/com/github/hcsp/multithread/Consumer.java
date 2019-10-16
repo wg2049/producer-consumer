231c298882b55209586feddd14142acbd8035dc3
wangjie_fourth <wangjie_fourth@163.com>
2019-10-16 23:05:37 +0800

    多线程在编辑共享变量时，while跟if有区别啊

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,30 @@
 package com.github.hcsp.multithread;
 
 public class Consumer extends Thread {
+    private Food food;
+    private Object lock;
+
+
+    public Consumer(Food food, Object lock) {
+        this.food = food;
+        this.lock = lock;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (lock) {
+                while (food.getFood() == null) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Consuming " + food.getFood());
+                food.setFood(null);
+                lock.notify();
+            }
+        }
+    }
 }
---
+++ src/main/java/com/github/hcsp/multithread/Food.java
@@ -0,0 +1,24 @@
+package com.github.hcsp.multithread;
+
+/**
+ * @ClassName Food
+ * @Description
+ * @Author 25127
+ * @Date 2019/10/16 22:28
+ * @Email jie.wang13@hand-china.com
+ **/
+public class Food {
+    private Integer food;
+
+    public Food(Integer food) {
+        this.food = food;
+    }
+
+    public Integer getFood() {
+        return food;
+    }
+
+    public void setFood(Integer food) {
+        this.food = food;
+    }
+}
