ce3650a6ef3bb811ad5ea7e687dc1a4d7fa446cf
Yuanmeng Xia <xymreasonxxs@163.com>
2019-10-22 21:45:52 +0800

    producer&consumer model (#37)
    producer&consumer model (#37)

--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,29 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Producer extends Thread {
+    BlockingQueue<Integer> queue;
+    BlockingQueue<Integer> signalQueue;
+
+    public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            int r = ThreadLocalRandom.current().nextInt();
+            System.out.println("Producing " + r);
+            try {
+                queue.put(r);
+                signalQueue.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
