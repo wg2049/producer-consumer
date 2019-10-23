6ac95356b8d06204fc6eb3e09642d67606984d5f
chenguoqing <42904687+acgq@users.noreply.github.com>
2019-10-23 16:51:21 +0800

    使用BlockingQueue实现生产者和消费者模式

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,31 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingQueue;
+
 public class Consumer extends Thread {
+
+    BlockingQueue<Integer> queue;
+    BlockingQueue<Integer> signal;
+
+
+    public Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signal) {
+        this.queue = queue;
+        this.signal = signal;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            Integer take = null;
+            try {
+                take = queue.take();
+                System.out.println("Consuming " + take);
+                //发送消息继续生产
+                signal.put(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,30 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+
 public class Producer extends Thread {
+    BlockingQueue<Integer> queue;
+    BlockingQueue<Integer> signal;
+
+    public Producer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signal) {
+        this.queue = queue;
+        this.signal = signal;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            int random = new Random().nextInt();
+            System.out.println("Producing " + random);
+            try {
+                queue.put(random);
+                signal.take();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
