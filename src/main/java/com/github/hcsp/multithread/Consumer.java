bcc6bcd0b8c405dbd142db9be866dba08a9004cb
Bigbig Zheng <39644918+zhengbigbig@users.noreply.github.com>
2019-10-28 18:10:08 +0800

    Lock/Condition (#41)
    Lock/Condition (#41)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,26 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingQueue;
+
 public class Consumer extends Thread {
+    BlockingQueue queue;
+    BlockingQueue signal;
+
+    public Consumer(BlockingQueue queue, BlockingQueue signal) {
+        this.queue = queue;
+        this.signal = signal;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                System.out.println("Consuming " + queue.take());
+                signal.put(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 }
--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,28 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+
 public class Producer extends Thread {
+    BlockingQueue queue;
+    BlockingQueue signal;
+
+    public Producer(BlockingQueue queue, BlockingQueue signal) {
+        this.queue = queue;
+        this.signal = signal;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                int r = new Random().nextInt();
+                System.out.println("Producing " + r);
+                queue.put(r);
+                signal.take();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
