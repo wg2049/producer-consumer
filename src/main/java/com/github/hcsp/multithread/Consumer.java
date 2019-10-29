87db34ff4836fd28e1d0ef4e0b2943591c81a5e6
phlvance <39873702+phlvance@users.noreply.github.com>
2019-10-29 17:15:54 +0800

    multithread producer-consumer model (#43)
    multithread producer-consumer model (#43)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,27 @@
 package com.github.hcsp.multithread;
 
+import java.util.concurrent.BlockingQueue;
+
 public class Consumer extends Thread {
+    BlockingQueue<Integer> queue;
+    BlockingQueue<Integer> signalQueue;
+
+    public Consumer(BlockingQueue<Integer> queue, BlockingQueue<Integer> signalQueue) {
+        this.queue = queue;
+        this.signalQueue = signalQueue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            int v = 0;
+            try {
+                v = queue.take();
+                signalQueue.put(0);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Consuming " + v);
+        }
+    }
 }
