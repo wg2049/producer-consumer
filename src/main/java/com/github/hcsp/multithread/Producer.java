f52ce9eefc22063d8f9788c9440e56b24951e938
Kelvin Chen <15088558209@163.com>
2019-11-03 00:24:39 +0800

    lock/condition实现 (#45)
    lock/condition实现 (#45)

--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,52 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+import java.util.Stack;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author Kelvin Chen
+ */
 public class Producer extends Thread {
+
+    private final Lock lock;
+    private final Condition emptyCondition;
+    private final Condition fullCondition;
+    private final int SIZE = 10;
+    private Stack<Integer> ret;
+
+    public Producer(Stack<Integer> ret, Lock lock, Condition emptyCondition, Condition fullCondition) {
+        this.ret = ret;
+        this.lock = lock;
+        this.emptyCondition = emptyCondition;
+        this.fullCondition = fullCondition;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < SIZE; i++) {
+            try {
+                lock.lock();
+                //满位
+                while (!ret.empty()) {
+                    fullCondition.await();
+                }
+
+                ret.push(getRandomInt());
+                emptyCondition.signalAll();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+
+    public int getRandomInt() {
+        int randomInt = new Random().nextInt();
+        System.out.println("Producing " + randomInt);
+        return randomInt;
+    }
 }
