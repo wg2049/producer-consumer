63062941f96ba2facd4a6d5c3230ea1f6e79c3e8
yrucrew <1622077015@qq.com>
2019-11-16 11:11:23 +0800

    update (#56)
    update (#56)

--- src/main/java/com/github/hcsp/multithread/Boss.java
+++ src/main/java/com/github/hcsp/multithread/Boss.java
@@ -1,27 +1,29 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Boss {
+
     public static void main(String[] args) throws InterruptedException {
-        // 请实现一个生产者/消费者模型，其中：
-        // 生产者生产10个随机的整数供消费者使用（随机数可以通过new Random().nextInt()获得）
-        // 使得标准输出依次输出它们，例如：
-        // Producing 42
-        // Consuming 42
-        // Producing -1
-        // Consuming -1
-        // ...
-        // Producing 10086
-        // Consuming 10086
-        // Producing -12345678
-        // Consuming -12345678
+        Queue<Integer> queue = new LinkedList<>();
 
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        for (int i = 0; i < 1; i++) {
+            new Thread(new Producer(queue)).start();
+            new Thread(new Consumer(queue)).start();
+        }
 
-        producer.start();
-        consumer.start();
-
-        producer.join();
-        producer.join();
     }
+    // 请实现一个生产者/消费者模型，其中：
+    // 生产者生产10个随机的整数供消费者使用（随机数可以通过new Random().nextInt()获得）
+    // 使得标准输出依次输出它们，例如：
+    // Producing 42
+    // Consuming 42
+    // Producing -1
+    // Consuming -1
+    // ...
+    // Producing 10086
+    // Consuming 10086
+    // Producing -12345678
+    // Consuming -12345678
 }
--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,30 @@
 package com.github.hcsp.multithread;
 
+import java.util.Queue;
+
 public class Consumer extends Thread {
+
+    private final Queue<Integer> queue;
+
+    public Consumer(Queue queue) {
+        this.queue = queue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (queue) {
+                while (queue.isEmpty()) {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Integer value = queue.poll();
+                System.out.println("Consuming " +value);
+                queue.notify();
+            }
+        }
+    }
 }
