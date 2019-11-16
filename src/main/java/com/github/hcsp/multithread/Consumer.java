5b6e0d121c1206eff12bcec3a7b486a5a68a3f88
jjs7716 <53330451+jjs7716@users.noreply.github.com>
2019-11-16 14:23:48 +0800

    synchronized方式 (#57)
    synchronized方式 (#57)

--- src/main/java/com/github/hcsp/multithread/Consumer.java
+++ src/main/java/com/github/hcsp/multithread/Consumer.java
@@ -1,6 +1,27 @@
 package com.github.hcsp.multithread;
 
+import java.util.Random;
+
+
 public class Consumer extends Thread {
+    private int size=10;
     @Override
-    public void run() {}
+    public void run() {
+        while (size > 0) {
+            synchronized (Boss.class) {
+                if (Boss.flag) {
+                    System.out.println("Consuming " + new Random().nextInt());
+                    Boss.flag = false;
+                    size--;
+                    Boss.class.notify();
+                } else {
+                    try {
+                        Boss.class.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 }
