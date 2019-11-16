0ddcf6d3aa5bc6f193deeb5b2d83019da1c6e45e
yrucrew <yrucrew@gmail.com>
2019-11-16 15:16:47 +0800

    Yrucrew (#58)
    Yrucrew (#58)

--- src/main/java/com/github/hcsp/multithread/Boss.java
+++ src/main/java/com/github/hcsp/multithread/Boss.java
@@ -1,27 +1,37 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Boss {
     public static void main(String[] args) throws InterruptedException {
-        // 请实现一个生产者/消费者模型，其中：
-        // 生产者生产10个随机的整数供消费者使用（随机数可以通过new Random().nextInt()获得）
-        // 使得标准输出依次输出它们，例如：
-        // Producing 42
-        // Consuming 42
-        // Producing -1
-        // Consuming -1
-        // ...
-        // Producing 10086
-        // Consuming 10086
-        // Producing -12345678
-        // Consuming -12345678
+        Queue<Integer> queue = new LinkedList<>();
+        Lock lock = new ReentrantLock();
+        Condition queueEmpty = lock.newCondition();
+        Condition queueFull = lock.newCondition();
 
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Thread producer = new Thread(new Producer2(queue, lock, queueEmpty, queueFull));
+        Thread consumer = new Thread(new Consumer2(queue, lock, queueEmpty, queueFull));
 
         producer.start();
         consumer.start();
 
         producer.join();
-        producer.join();
+        consumer.join();
     }
+    // 请实现一个生产者/消费者模型，其中：
+    // 生产者生产10个随机的整数供消费者使用（随机数可以通过new Random().nextInt()获得）
+    // 使得标准输出依次输出它们，例如：
+    // Producing 42
+    // Consuming 42
+    // Producing -1
+    // Consuming -1
+    // ...
+    // Producing 10086
+    // Consuming 10086
+    // Producing -12345678
+    // Consuming -12345678
 }
--- src/main/java/com/github/hcsp/multithread/Producer.java
+++ src/main/java/com/github/hcsp/multithread/Producer.java
@@ -1,6 +1,32 @@
 package com.github.hcsp.multithread;
 
+import java.util.Queue;
+import java.util.Random;
+
 public class Producer extends Thread {
+
+    private final Queue<Integer> queue;
+
+    public Producer(Queue queue) {
+        this.queue = queue;
+    }
+
     @Override
-    public void run() {}
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            synchronized (queue) {
+                while (!queue.isEmpty()) {
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                int random = new Random().nextInt();
+                queue.offer(random);
+                System.out.println("Producing " + random);
+                queue.notify();
+            }
+        }
+    }
 }
