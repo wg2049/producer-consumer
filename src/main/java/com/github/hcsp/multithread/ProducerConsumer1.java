02beaeb1a168606ab98e016ba36f51cf6c787f91
BirdSnail <39896910+BirdSnail@users.noreply.github.com>
2019-11-20 17:26:58 +0800

    多种方式实现生产消费者模型 (#66)
    多种方式实现生产消费者模型 (#66)

--- src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer1.java
@@ -1,24 +1,86 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * 使用原生的 'synchronized' 'wait/notify'机制完成生产者消费
+ */
 public class ProducerConsumer1 {
     public static void main(String[] args) throws InterruptedException {
-        Producer producer = new Producer();
-        Consumer consumer = new Consumer();
+        Resource resource = new Resource();
+
+        Producer producer = new Producer(resource);
+        Consumer consumer = new Consumer(resource);
 
         producer.start();
         consumer.start();
 
         producer.join();
-        producer.join();
+        consumer.join();
     }
 
     public static class Producer extends Thread {
+        private Resource resource;
+
+        public Producer(Resource resource) {
+            this.resource = resource;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    resource.produceResource(ThreadLocalRandom.current().nextInt());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Resource resource;
+
+        public Consumer(Resource resource) {
+            this.resource = resource;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    resource.consumeResource();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 代表一个线程需要操作的资源类，资源存放在队列中
+     */
+    private static class Resource {
+        private Queue<Integer> queue = new LinkedList<>();
+
+        private synchronized void produceResource(int num) throws InterruptedException {
+            while (!queue.isEmpty()) {
+                this.wait();
+            }
+            System.out.println("Producing " + num);
+            queue.add(num);
+            this.notifyAll();
+        }
+
+        private synchronized void consumeResource() throws InterruptedException {
+            while (queue.isEmpty()) {
+                this.wait();
+            }
+            System.out.println("Consuming " + queue.remove());
+            this.notifyAll();
+        }
     }
 }
--- src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
+++ src/main/java/com/github/hcsp/multithread/ProducerConsumer2.java
@@ -1,24 +1,104 @@
 package com.github.hcsp.multithread;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用{@link Lock} ,{@link Condition}完成生产消费者模型
+ */
 public class ProducerConsumer2 {
-    //    public static void main(String[] args) throws InterruptedException {
-    //        Producer producer = new Producer();
-    //        Consumer consumer = new Consumer();
-    //
-    //        producer.start();
-    //        consumer.start();
-    //
-    //        producer.join();
-    //        producer.join();
-    //    }
+    public static void main(String[] args) throws InterruptedException {
+        Resource resource = new Resource();
+
+        Producer producer = new Producer(resource);
+        Consumer consumer = new Consumer(resource);
+
+        producer.start();
+        consumer.start();
+
+        producer.join();
+        consumer.join();
+    }
 
     public static class Producer extends Thread {
+        private Resource resource;
+
+        Producer(Resource resource) {
+            this.resource = resource;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    resource.produceResource(ThreadLocalRandom.current().nextInt());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     public static class Consumer extends Thread {
+        private Resource resource;
+
+        Consumer(Resource resource) {
+            this.resource = resource;
+        }
+
         @Override
-        public void run() {}
+        public void run() {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    resource.consumeResource();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /**
+     * 代表一个线程需要操作的资源类，资源存放在队列中
+     */
+    private static class Resource {
+        private Queue<Integer> queue = new LinkedList<>();
+
+        // ---------- lock ---------------
+        private Lock lock = new ReentrantLock();
+        private Condition canProduce = lock.newCondition();
+        private Condition canConsume = lock.newCondition();
+
+        private void produceResource(int num) throws InterruptedException {
+            lock.lock();
+            try {
+                while (!queue.isEmpty()) {
+                    canProduce.await();
+                }
+                System.out.println("Producing " + num);
+                queue.add(num);
+                canConsume.signalAll();
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        private void consumeResource() throws InterruptedException {
+            lock.lock();
+            try {
+                while (queue.isEmpty()) {
+                    canConsume.await();
+                }
+                System.out.println("Consuming " + queue.remove());
+                canProduce.signalAll();
+            } finally {
+                lock.unlock();
+            }
+
+        }
     }
 }
